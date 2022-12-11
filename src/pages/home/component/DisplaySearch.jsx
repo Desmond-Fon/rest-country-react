b1ad380800ce245533501fa166c6f9c639bc4a4c
Desmond <desmondfon@yahoo.com>
2022-12-11 16:01:57 +0100

    implemented the search function

---
+++ src/pages/home/component/DisplaySearch.jsx
@@ -0,0 +1,26 @@
+function DisplaySearch ({searchCountry}) {
+    return ( <>
+      <div>
+        {searchCountry.map((names) =>
+        <div className="flex flex-col justify-center items-center">
+        <div className="w-4/5 bg-whiteLMDM mb-9 rounded-[5px] shadow-md text-left dark:text-whiteLMDM dark:bg-darkBlue hello">
+        
+        <div className='w-full'>
+        <img src={names.flags.svg} alt="" className="rounded-t-[5px] w-full h-full" />
+        </div>
+        <div className="pb-10 pt-7 text-left h-3/5 ml-5">
+            <h2 className="text-lg font-bold pb-4">{names.name.common}</h2>
+            <p className="font-semibold">Population: <span className="font-light">{names.population}</span></p>
+            <p className="font-semibold">Region: <span className="font-light">{names.region}</span></p>
+            <p className="font-semibold">Capital: <span className="font-light">{names.capital}</span></p>
+        </div>
+      
+      </div>
+      </div>
+    )
+    } 
+        </div>
+    </> );
+}
+ 
+export default DisplaySearch;
--- src/pages/home/component/SearchBar.jsx
+++ src/pages/home/component/SearchBar.jsx
@@ -1,12 +1,12 @@
 // import { useState } from 'react';
 
-const SearchBar = ({name, searchName, setName}) => {
+const SearchBar = ({name, setName}) => {
 
    
 
     return ( <div className="mb-10">
-   <form action=""  >
-   <input type="search" name="search" placeholder="Search for a country..." className="text-sm p-4 rounded-[5px] shadow-md  w-full pl-8 dark:bg-darkBlue dark:text-whiteLMDM outline-none" value={name}  onChange={(e) => setName(e.target.value)} onKeyPress={searchName}/>
+   <form>
+   <input type="search" name="search" placeholder="Search for a country..." className="text-sm p-4 rounded-[5px] shadow-md  w-full pl-8 dark:bg-darkBlue dark:text-whiteLMDM outline-none" value={name}  onChange={(userSearch) => setName(userSearch.target.value)} />
    </form>
 </div> );
 }
